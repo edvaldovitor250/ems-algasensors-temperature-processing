1a4a8988d70d8e84bcbd530fb87751aa66710674
Edvaldo <edvaldovitor250@gmail.com>
2025-04-19 22:12:04 -0300

    feat(temperature-processing):Implementing Temperature Processing Microservices

---
+++ src/main/java/com/algaworks/algasensors/temperatureprocessing/api/config/web/WebConfig.java
@@ -0,0 +1,12 @@
+package com.algaworks.algasensors.temperatureprocessing.api.config.web;
+
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new StringToTSIDWebConverter());
+    }
+}
---
+++ src/main/java/com/algaworks/algasensors/temperatureprocessing/api/controller/TemperatureProcessingController.java
@@ -0,0 +1,40 @@
+package com.algaworks.algasensors.temperatureprocessing.api.controller;
+
+import com.algaworks.algasensors.temperatureprocessing.api.model.TemperatureLogOutPut;
+import com.algaworks.algasensors.temperatureprocessing.coomon.IdGenerator;
+import io.hypersistence.tsid.TSID;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.OffsetDateTime;
+
+@RestController
+@RequestMapping("/api/sensors/{sensorId}/temperatures/data")
+@Slf4j
+public class TemperatureProcessingController {
+
+    @PostMapping(consumes = MediaType.TEXT_PLAIN_VALUE)
+    public void data(@PathVariable TSID sensorId, @RequestBody String input) {
+
+        if (input == null || input.isBlank()) {
+            throw new IllegalArgumentException("Input cannot be null or empty");
+        }
+
+        Double temperature;
+        try {
+            temperature = Double.parseDouble(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Input is not a number");
+        }
+
+        TemperatureLogOutPut temperatureLog = TemperatureLogOutPut.builder()
+                .id(IdGenerator.generateId())
+                .sensorId(sensorId)
+                .value(temperature)
+                .registeredAt(OffsetDateTime.now())
+                .build();
+
+        log.info(temperatureLog.toString());
+    }
+}
